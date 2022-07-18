65e81268c8af0a93393d34a225d4eb8bc46967b1
soumyyyaaa <soumya.agarwal1103@gmail.com>
2022-07-18 10:10:52 +0530

    Technology Page Made Responsive

--- Technology/technology.js
+++ Technology/technology.js
@@ -4,7 +4,7 @@ $(".space-port").click(function(){
     $(".space-port").addClass("active-button");
     $(".heading1").text("SPACEPORT");
     $(".plain-text").text("A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.")
-    $(".tech-img").attr("src", "/Technology/image-spaceport-portrait.jpg")
+    $(".tech-img-portrait").attr("src", "/Technology/image-spaceport-portrait.jpg")
 });
 
 $(".space-capsule").click(function(){
@@ -13,5 +13,20 @@ $(".space-capsule").click(function(){
     $(".space-capsule").addClass("active-button");
     $(".heading1").text("SPACE CAPSULE");
     $(".plain-text").text("A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained..")
-    $(".tech-img").attr("src", "/Technology/image-space-capsule-portrait.jpg")
+    $(".tech-img-portrait").attr("src", "/Technology/image-space-capsule-portrait.jpg")
+});
+
+$(window).resize(function() {
+    var windowWidth = $(window).width();
+    if (windowWidth < '600') {
+        $(".toggler-button-tech").removeClass("buttons");
+    }
+});
+
+$(".space-port").click(function(){
+    $(".tech-img-landscape").attr("src", "/Technology/image-spaceport-landscape.jpg");
+});
+
+$(".space-capsule").click(function(){
+    $(".tech-img-landscape").attr("src", "/Technology/image-space-capsule-landscape.jpg")
 });
